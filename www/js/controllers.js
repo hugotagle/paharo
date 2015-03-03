957723c87a2e473b63727081fd26a21bb7a12ad4
Hugo <hugo@Nostromo.local>
2015-03-03 13:03:55 -0600

    local storage howto

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,27 @@
 angular.module('paharo.controllers', [])
 
-.controller('SplashController', ['$scope', '$state', function ($scope, $state) {
+.controller('SplashController', ['$scope', '$state', '$ionicViewService', function ($scope, $state, $ionicViewService ) {
+    
+    // see if user is registered
+    if(window.localStorage.getItem("registered") === "undefined" || window.localStorage.getItem("registered") === null) {
+    
+        alert('registered is undefined or null');
+
+        $ionicViewService.nextViewOptions({
+            disableAnimate: true,
+            disableBack: true
+        });
+        // must register
+        $state.go('register');
+    }
+    
+    // pretend is stored
+    //window.localStorage.setItem("registered", null);
+        window.localStorage.removeItem("registered");
+
+    
+    $scope.status = "Making it this far means you are signed in";
+ 
     $scope.register = function () {
         $state.go('register');
     }
