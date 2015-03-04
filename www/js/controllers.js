505aba986b6b2b3492c832a4f5cf3dfc9c882901
Hugo <hugo@Nostromo.local>
2015-03-03 21:07:23 -0600

    login controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,31 +1,37 @@
 angular.module('paharo.controllers', [])
 
-.controller('SplashController', ['$scope', '$state', '$ionicViewService', function ($scope, $state, $ionicViewService ) {
-    
-    // see if user is registered
-    if(window.localStorage.getItem("registered") === "undefined" || window.localStorage.getItem("registered") === null) {
-    
-        alert('registered is undefined or null');
+.controller('SplashController', ['$scope', '$state', '$ionicViewService', function ($scope, $state, $ionicViewService) {
 
-        $ionicViewService.nextViewOptions({
-            disableAnimate: true,
-            disableBack: true
-        });
-        // must register
-        $state.go('register');
-    }
-    
-    // pretend is stored
-    //window.localStorage.setItem("registered", null);
-        window.localStorage.removeItem("registered");
+        // see if user is registered
+//        if (window.localStorage.getItem("registered") === "undefined" || window.localStorage.getItem("registered") === null) {
+//
+//            $ionicViewService.nextViewOptions({
+//                disableAnimate: true,
+//                disableBack: true
+//            });
+//            // must register
+//            $state.go('register');
+//        }
 
-    
-    $scope.status = "Making it this far means you are signed in";
- 
-    $scope.register = function () {
-        $state.go('register');
-    }
-    $scope.login = function () {
-        $state.go('login');
-    }
+        // pretend is stored
+        //window.localStorage.setItem("registered", null);
+        //window.localStorage.removeItem("registered");
+
+//        $scope.status = "Making it this far means you are signed in";
+
+        $scope.register = function () {
+            $state.go('register');
+        }
+        $scope.login = function () {
+            $state.go('login');
+        }
+}])
+    //
+    // Login
+    //
+    .controller('LoginController', ['$scope', '$state', '$ionicViewService', function ($scope, $state, $ionicViewService) {
+        
+        $scope.login = function () {
+            alert('Login');
+        }
 }]);
