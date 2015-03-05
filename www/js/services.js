66f0f8afa381513e383921135c9443a3f84470bf
Hugo <hugo@Nostromo.local>
2015-03-05 09:37:53 -0600

    GlobalPC service

--- www/js/services.js
+++ www/js/services.js
@@ -1,13 +1,11 @@
-angular.module('paharo.services', []);
+angular.module('paharo.services', [])
 
-//    $scope.getData = function() {
-//        $http.get("http://localhost/example.json", { params: { "username": "value1", "password": "value2" } })
-//            .success(function(data) {
-//                $scope.firstname = data.firstname;
-//                $scope.lastname = data.lastname;
-//            })
-//            .error(function(data) {
-//                alert("ERROR");
-//            });
-//    }
- 
+.factory('GlobalPC', ['$http', 'PARSE_CREDENTIALS', function ($http, PARSE_CREDENTIALS) {
+    return {
+        authenticate: function () {
+            return $http.post('https://localhost/_dev/api/', data, {
+                headers: {}
+            });
+        }
+    }
+}]);
