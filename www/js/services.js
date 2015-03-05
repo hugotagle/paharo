d37c56e5a247db8bd54d481d4e2738b684932a29
Hugo <hugo@Nostromo.local>
2015-03-05 07:41:11 -0600

    First commit

---
+++ www/js/services.js
@@ -0,0 +1,13 @@
+angular.module('paharo.services', []);
+
+//    $scope.getData = function() {
+//        $http.get("http://localhost/example.json", { params: { "username": "value1", "password": "value2" } })
+//            .success(function(data) {
+//                $scope.firstname = data.firstname;
+//                $scope.lastname = data.lastname;
+//            })
+//            .error(function(data) {
+//                alert("ERROR");
+//            });
+//    }
+ 
