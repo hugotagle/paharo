a61fcf87afb85981c544b6ec49c40f2aff90927f
Hugo <hugo@Nostromo.local>
2015-03-07 08:01:03 -0600

    get profile working

--- www/js/services.js
+++ www/js/services.js
@@ -3,27 +3,34 @@ angular.module('paharo.services', [])
 .factory('GlobalPC', ['$http', function ($http) {
     return {
         authenticate: function (data) {
+            
             var queryString = 'username=' + data.username + '&password=' + data.password;
-            //alert(queryString);
+            alert(queryString);
 
             $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
             return $http.post('http://' + window.localStorage.getItem('host') + '/_dev/api/', queryString, {
                 headers: {}
             });
+        },
+        getProfile: function (data) {
+
+            return $http.get('http://localhost/_dev/api/?method=profile&format=json&authKey=', data, {
+                headers: {}
+            });
         }
     }
 
-//    return {
-//        getProfile: function (data) {
-//                var queryString = 'username=' + data.username + '&password=' + data.password;
-//                //alert(queryString);
-//
-//                //$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
-//                return $http.get('http://localhost/_dev/api/?method=profile&format=json&authKey=', queryString, {
-//                    headers: {}
-//                });
-//            }
-//            //http://localhost/_dev/api/?method=profile&format=json&authKey=8b29356b1c66f9b6f113fc47f5274c27
-//    }
+    //    return {
+    //        getProfile: function (data) {
+    //                var queryString = 'username=' + data.username + '&password=' + data.password;
+    //                //alert(queryString);
+    //
+    //                //$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+    //                return $http.get('http://localhost/_dev/api/?method=profile&format=json&authKey=', queryString, {
+    //                    headers: {}
+    //                });
+    //            }
+    //            //http://localhost/_dev/api/?method=profile&format=json&authKey=8b29356b1c66f9b6f113fc47f5274c27
+    //    }
 
 }]);
