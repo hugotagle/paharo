598d6f8723693aef9c752392c1db294c90188a1e
Hugo <hugo@Nostromo.local>
2015-03-09 09:10:23 -0500

    change my local api directory

--- www/js/services.js
+++ www/js/services.js
@@ -8,13 +8,13 @@ angular.module('paharo.services', [])
             //alert(queryString);
 
             $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
-            return $http.post('http://' + window.localStorage.getItem('host') + '/_dev/api/', queryString, {
+            return $http.post('http://' + window.localStorage.getItem('host') + '/pc/api/', queryString, {
                 headers: {}
             });
         },
         getProfile: function (data) {
 
-            var fullUrl = 'http://' + window.localStorage.getItem('host') + '/_dev/api/?method=profile&format=json&authKey='+data.authKey;
+            var fullUrl = 'http://' + window.localStorage.getItem('host') + '/pc/api/?method=profile&format=json&authKey='+data.authKey;
             //alert(fullUrl);
             
             return $http.get(fullUrl);
